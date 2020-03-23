7742da41e68dbdfc6b9ac8e46611ad1ebe5f57c4
rnkwilliams <rnkwilliams07@gmail.com>
2020-03-23 12:45:14 -0500

    add seed data and post controller actions

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,13 +1,34 @@
 class Api::V1::PostsController < ApplicationController
     def index
-        posts = Post.all
-        render json: posts, status: 200
+        @posts = Post.all
+        render json: @posts, status: 200
     end
 
     def show
-        post = Post.find(params[:id])
-        render json: posts, status: 200
+        @post = Post.find(params[:id])
+        render json: @post, status: 200
     end
 
-   
+    def create
+        @post = Post.create(post_params)
+        render json: @post, status: 200
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        @post.update(post_params)
+        render json: @post, status: 200
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.delete
+        render json: {postId: @post.id}
+    end
+
+   private
+
+   def post_params
+    params.require(:post).permit(:date, :content1, :content2, :content3)
+   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+post1 = Post.create(date: "3/23/20", content1: "I am grateful for having a loving family and being in perfect health.", content2: "I am a better version of myself than yesterday.", content3: "Today could have been better if I would have read my book.", likes: 0 )
+post2 = Post.create(date: "3/22/20", content1: "I am grateful for waking up this morning.", content2: "I am strong.", content3: "Today could have been better if I would have ran today.", likes: 0 )
+post3 = Post.create(date: "3/21/20", content1: "I am grateful for my parents.", content2: "I am more than enough.", content3: "Today could have been better if I would have stayed home.", likes: 0 )
