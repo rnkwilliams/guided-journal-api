b3b71c3fef8eb198f49b5aab5e70c7905fc5ee6b
rnkwilliams <rnkwilliams07@gmail.com>
2020-03-24 18:07:29 -0500

    add category model, migrations, seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_164800) do
+ActiveRecord::Schema.define(version: 2020_03_24_223457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "date"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+daily_life = Category.create(name: "Daily Life")
+self_care = Category.create(name: "Self Care")
+goal_setting = Category.create(name: "Goal Setting")
 
-
-post1 = Post.create(date: "3/23/20", content1: "I am grateful for having a loving family and being in perfect health.", content2: "I am a better version of myself than yesterday.", content3: "Today could have been better if I would have read my book.", likes: 0 )
-post2 = Post.create(date: "3/22/20", content1: "I am grateful for waking up this morning.", content2: "I am strong.", content3: "Today could have been better if I would have ran today.", likes: 0 )
-post3 = Post.create(date: "3/21/20", content1: "I am grateful for my parents.", content2: "I am more than enough.", content3: "Today could have been better if I would have stayed home.", likes: 0 )
+post1 = Post.create(category_id: self_care.id, date: "3/23/20", content1: "I am grateful for having a loving family and being in perfect health.", content2: "I am a better version of myself than yesterday.", content3: "Today could have been better if I would have read my book.", likes: 0 )
+post2 = Post.create(category_id: self_care.id, date: "3/22/20", content1: "I am grateful for waking up this morning.", content2: "I am strong.", content3: "Today could have been better if I would have ran today.", likes: 0 )
+post3 = Post.create(category_id: self_care.id, date: "3/21/20", content1: "I am grateful for my parents.", content2: "I am more than enough.", content3: "Today could have been better if I would have stayed home.", likes: 0 )
