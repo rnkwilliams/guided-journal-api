bbeaf59463f634b21c60f199c2ec25166de1605a
rnkwilliams <rnkwilliams07@gmail.com>
2020-04-01 11:24:31 -0500

    refactor posts controller actions and added serializer gem

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,38 +1,41 @@
 class Api::V1::PostsController < ApplicationController
     def index
-        @posts = Post.all
-        render json: @posts, status: 200
+        posts = Post.all
+        render json: posts, except: [:created_at, :updated_at]
     end
 
     def show
-        @post = Post.find(params[:id])
-        render json: @post, status: 200
+        post = Post.find_by(id: params[:id])
+        if post
+            render json: post, except: [:created_at, :updated_at]
+        else
+            render json: {message: 'Post not found'}
+        end
     end
 
     def create
-        @category = Category.find_by(id: post_params["category_id"])
+        category = Category.find_by(id: post_params["category_id"])
 
-        @post = Post.new(post_params)
-        @post.category = @category
+        post = Post.new(post_params)
+        post.category = category
 
-        if @post.save!
-            render json: @post, status: 200
+        if post.save!
+            render json: post, status: 200
         else
-            render json: @post.errors, status: :unprocessable_entity
-        end
-        
+            render json: {message: 'Post did not save'}
+        end  
     end
 
     def update
-        @post = Post.find(params[:id])
-        @post.update(post_params)
-        render json: @post, status: 200
+        post = Post.find_by(id: params[:id])
+        post.update(post_params)
+        render json: post, status: 200
     end
 
     def destroy
-        @post = Post.find(params[:id])
-        @post.delete
-        render json: {postId: @post.id}
+        post = Post.find_by(id: params[:id])
+        post.delete
+        render json: {postId: post.id}
     end
 
    private
